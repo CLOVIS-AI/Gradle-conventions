7a0a265fc678bc3c51f8be5be2cd267f4f21e32a
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2023-11-01 19:58:14 +0100

    test(base): Increase the test timeout

--- base/src/test/kotlin/BasePlugins.kt
+++ base/src/test/kotlin/BasePlugins.kt
@@ -2,9 +2,11 @@ package dev.opensavvy.conventions
 
 import io.kotest.core.spec.style.StringSpec
 import opensavvy.prepared.runner.kotest.preparedSuite
+import opensavvy.prepared.suite.config.CoroutineTimeout
+import kotlin.time.Duration.Companion.minutes
 
 class BasePlugins : StringSpec({
-	preparedSuite {
+	preparedSuite(CoroutineTimeout(1.minutes)) {
 		baseTests()
 	}
 })
