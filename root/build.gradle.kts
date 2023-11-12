777d169860e653a347c7c74d2ab8d401fa457749
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2023-11-12 16:58:12 +0100

    feat(root): The root plugin depends on the Kotlin plugin

--- root/build.gradle.kts
+++ root/build.gradle.kts
@@ -1,9 +1,22 @@
+import dev.opensavvy.conventions.versions.Versions
+
+buildscript {
+	dependencies {
+		classpath("dev.opensavvy.gradle.conventions:versions:$version")
+	}
+}
+
 plugins {
 	`kotlin-dsl`
 	`maven-publish`
 }
 
 dependencies {
+	// Not actually used, but needs to be part of the classpath of the root plugin to force
+	// this exact specific version.
+	// Required by the Kotlin and the Dokkatoo plugins.
+	implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.KOTLIN}")
+
 	api(libs.gradle.nexusPublish)
 	api(libs.gradle.dokkatoo)
 
