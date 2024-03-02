c9d8c0e0c572ee0dbb1d119a0cce9123ddaa83a0
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2024-03-02 11:59:42 +0100

    upgrade(versions): Compose Multiplatform 1.6.0

--- versions/src/main/kotlin/Versions.kt
+++ versions/src/main/kotlin/Versions.kt
@@ -11,7 +11,7 @@ object Versions {
 	 *
 	 * Compose Multiplatform versions depend on specific [KOTLIN] versions, so we must align both.
 	 */
-	const val COMPOSE_MULTIPLATFORM = "1.5.12"
+	const val COMPOSE_MULTIPLATFORM = "1.6.0"
 
 	/**
 	 * Version of Java used when creating libraries.
