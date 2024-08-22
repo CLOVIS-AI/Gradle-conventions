ea416e19624ae9110305d0ba0408b0085d6a130e
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2024-08-22 20:54:49 +0200

    feat(kotlin): All Kotlin projects now use Power Assert, not just libraries

--- kotlin/src/main/kotlin/base.gradle.kts
+++ kotlin/src/main/kotlin/base.gradle.kts
@@ -1,9 +1,30 @@
 package dev.opensavvy.conventions.kotlin
 
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+
 plugins {
 	kotlin("multiplatform")
+	id("org.jetbrains.kotlin.plugin.power-assert")
 }
 
 tasks.withType<Test>().configureEach {
 	useJUnitPlatform()
+}
+
+@OptIn(ExperimentalKotlinGradlePluginApi::class)
+powerAssert {
+	functions = listOf(
+		// Standard library
+		"kotlin.assert",
+		"kotlin.require",
+		"kotlin.requireNotNull",
+		"kotlin.check",
+		"kotlin.checkNotNull",
+
+		// Standard test library
+		"kotlin.test.assertTrue",
+		"kotlin.test.assertFalse",
+		"kotlin.test.assertEquals",
+		"kotlin.test.assertNull",
+	)
 }
