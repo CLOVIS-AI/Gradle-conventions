93f18ad7a9561770cb1fc9c051e0ee91d7ceb55f
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2025-07-13 17:20:02 +0200

    build(versions): Simplify the version embedding logic

--- versions/build.gradle.kts
+++ versions/build.gradle.kts
@@ -5,31 +5,25 @@ plugins {
 }
 
 val createVersion by tasks.registering(EmbedVersionTask::class) {
-	sources.set(project.layout.projectDirectory.file("src/").toString())
-	this.version = project.version.toString()
+	writePath.set(project.layout.projectDirectory.file("src/main/kotlin/Self.kt"))
+	version = project.version.toString()
 }
 
 abstract class EmbedVersionTask : DefaultTask() {
 
-	@get:Input
-	abstract val sources: Property<String>
-
-	@get:Internal
-	val writePath: Provider<String>
-		get() = sources.map { "$it/main/kotlin/Self.kt" }
+	@get:OutputFile
+	abstract val writePath: RegularFileProperty
 
 	@get:Input
 	abstract val version: Property<String>
 
 	init {
 		description = "Store the self version into the resources"
-
-		outputs.file(writePath)
 	}
 
 	@TaskAction
 	fun embedVersion() {
-		File(writePath.get()).writeText("""
+		writePath.get().asFile.writeText("""
 			package dev.opensavvy.conventions.versions
 			
 			const val OPENSAVVY_CONVENTIONS_VERSION = "${version.get()}"
