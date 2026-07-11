050236a4abff1c1acb1570365cefd3fff2942319
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2026-07-11 17:17:57 +0200

    refactor(plugin): Rewrite to stop using kotlin-dsl

--- plugin/src/main/kotlin/OpenSavvyPluginPlugin.kt
+++ plugin/src/main/kotlin/OpenSavvyPluginPlugin.kt
@@ -1,5 +1,6 @@
 package dev.opensavvy.conventions
 
+import org.gradle.api.HasImplicitReceiver
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.credentials.HttpHeaderCredentials
@@ -8,11 +9,14 @@ import org.gradle.api.publish.PublishingExtension
 import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.api.tasks.testing.Test
 import org.gradle.authentication.http.HttpHeaderAuthentication
-import org.gradle.kotlin.dsl.*
+import org.jetbrains.kotlin.samWithReceiver.gradle.SamWithReceiverExtension
 import tapmoc.TapmocExtension
 import java.net.URI
 
 class OpenSavvyPluginPlugin : Plugin<Project> {
+
+	// The Kotlin version displayed by './gradlew --version'
+	private val targetedKotlin = "2.3.0"
 
 	// https://gitlab.com/opensavvy/groundwork/prepared/-/releases
 	private val prepared = "2.5.3"
@@ -21,15 +25,20 @@ class OpenSavvyPluginPlugin : Plugin<Project> {
 
 	override fun apply(target: Project) {
 		target.pluginManager.apply("maven-publish")
-		target.pluginManager.apply("org.gradle.kotlin.kotlin-dsl")
+		target.pluginManager.apply("org.jetbrains.kotlin.jvm")
+		target.pluginManager.apply("org.jetbrains.kotlin.plugin.sam.with.receiver")
+		target.pluginManager.apply("java-gradle-plugin")
 		target.pluginManager.apply("com.gradleup.tapmoc")
 
-		target.extensions.configure<JavaPluginExtension> {
-			withSourcesJar()
-		}
+		val samWithReceiver = target.extensions.getByType(SamWithReceiverExtension::class.java)
+		samWithReceiver.annotation(HasImplicitReceiver::class.qualifiedName!!)
+
+		val java = target.extensions.getByType(JavaPluginExtension::class.java)
+		java.withSourcesJar()
 
 		val tapmoc = target.extensions.getByType(TapmocExtension::class.java)
 		tapmoc.java(javaCompatibility)
+		tapmoc.kotlin(targetedKotlin)
 
 		/*
 		 * When running in GitLab, uses the auto-created CI variables to configure the GitLab Maven Registry.
@@ -37,7 +46,7 @@ class OpenSavvyPluginPlugin : Plugin<Project> {
 		 * - https://docs.gitlab.com/ee/user/packages/maven_repository/
 		 * - https://docs.gitlab.com/ee/ci/variables/predefined_variables.html
 		 */
-		target.extensions.configure<PublishingExtension> {
+		target.extensions.getByType(PublishingExtension::class.java).apply {
 			repositories {
 				val projectId = System.getenv("CI_PROJECT_ID") ?: return@repositories
 				val token = System.getenv("CI_JOB_TOKEN") ?: return@repositories
@@ -53,38 +62,24 @@ class OpenSavvyPluginPlugin : Plugin<Project> {
 					}
 
 					authentication {
-						register<HttpHeaderAuthentication>("header")
+						register("header", HttpHeaderAuthentication::class.java)
 					}
 				}
 			}
 
 			publications.withType(MavenPublication::class.java) {
 				if (name.matches(Regex(".*PluginMarkerMaven"))) {
-					artifact(target.tasks["sourcesJar"])
+					artifact(target.tasks.getByName("sourcesJar"))
 				}
 			}
 		}
 
-		val version = target.provider {
-			val version = embeddedKotlinVersion.replaceAfterLast('.', "") // Remove the patch version
-				.removeSuffix(".")
-			target.logger.info("Setting the Kotlin compatibility target to $version")
-			@Suppress("EnumValuesSoftDeprecate") // '.entries' does not exist yet for the Kotlin version used in plugins
-			org.jetbrains.kotlin.gradle.dsl.KotlinVersion.values().find { it.version == version }
-				?: error("Could not set Kotlin compatibility for this plugin module to $version. Embedded Kotlin version: $embeddedKotlinVersion. Known versions: ${org.jetbrains.kotlin.gradle.dsl.KotlinVersion.values().map { it.version }}")
-		}
-
-		tapmoc.kotlin("${version.get().version}.0")
-
-		target.tasks.withType(Test::class.java) {
+		target.tasks.withType(Test::class.java).configureEach {
 			useJUnitPlatform()
 		}
 
-		val testImplementation by target.configurations.getting
-		target.dependencies {
-			testImplementation("dev.opensavvy.prepared:suite:$prepared")
-			testImplementation("dev.opensavvy.prepared:runner-kotlin-test:$prepared")
-			testImplementation("dev.opensavvy.prepared:compat-gradle:$prepared")
-		}
+		target.dependencies.add("testImplementation", "dev.opensavvy.prepared:suite:$prepared")
+		target.dependencies.add("testImplementation", "dev.opensavvy.prepared:runner-kotlin-test:$prepared")
+		target.dependencies.add("testImplementation", "dev.opensavvy.prepared:compat-gradle:$prepared")
 	}
 }
